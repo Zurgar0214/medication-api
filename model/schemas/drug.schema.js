b2b517c8e36c131646f3bfd521f36277f3139390
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-02-17 13:33:27 -0500

    Se creó el esquema para el medicamento

---
+++ model/schemas/drug.schema.js
@@ -0,0 +1,34 @@
+/** Packages */
+const mongoose = require("mongoose")
+
+/** Schema creation */
+const drugSchema = new mongoose.Schema({
+    code: {
+        type: "String",
+        required: true,
+        unique: true
+    },
+    name: {
+        type: "String",
+        required: true,
+    },
+    presentation: {
+        type: "String",
+        required: true
+    },
+    units: {
+        type: "String",
+        required
+    },
+    expDate: {
+        type: "String",
+        required: true
+    },
+    laboratoty: {
+        type: "String",
+        required: true
+    }
+})
+
+/** Schema exportation */
+module.exports = drugSchema
