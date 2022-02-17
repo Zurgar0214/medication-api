d9161e443ec8b073b1b73d83a0c526004db1231f
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-02-17 14:07:26 -0500

    Se creó el controlador para el medicamento

---
+++ controller/logic/drug.controller.js
@@ -0,0 +1,34 @@
+/** Dto */
+const drugDto = require("../../model/dto/drug.dto")
+const config = require("config")
+
+/** Entidity operations on data base */
+exports.getAll = (req, res, next) => {
+    drugDto.getAll({}, (err, data) => {
+        if(err) {
+            res.status(400).json( {error: err} )
+        } else {
+            res.status(200).json( {info: data} )
+        }
+    })
+}
+
+
+exports.createDrug = (req, res, next) => {
+    let drug = {
+        code: req.body.code,
+        name: req.body.name,
+        presentation: req.body.presentation,
+        units: req.body.units,
+        expDate: req.body.expDate,
+        laboratoty: req.body.laboratoty
+    }
+
+    drugDto.create(drug, (err, data) => {
+        if(err) {
+            res.status(400).json( {error: err} )
+        } else {
+            res.status(200).json( {res: "ok", info: data} )
+        }
+    })
+}
