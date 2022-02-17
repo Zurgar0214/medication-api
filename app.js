ed1c9360d22a5a25b7d61e5db95300c47c94a06a
unknown <jdiegov15@gmail.com>
2022-02-17 13:49:42 -0500

    Se implementa el app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+/**
+ * mongodb+srv://medication_rest_api:LZtCJ46PA4nwAKHI@cluster0.eunjr.mongodb.net/dbMedication
+ */
+
+/** Packages */
+const express = require("express")
+const config = require("config")
+const bodyParser = require("bordy-parser")
+
+/** App Configuration */
+const app = express();
+const port = config.get("server-port");
+const jsonParser = bodyParser.json();
+const urlEncodedParser = bodyParser.urlencoded(
+    { extended: true }
+)
+
+app.use(jsonParser);
+app.use(urlEncodedParser);
+
+const ipFn = require("./middleware/getIpAddress");
+app.use("*", ipFn);
+
+/** Methods */
+app.get("/", (req, res, next) => {
+    res.send("Welcome to medication rest api.");
+})
+
+// Drug Routes Loading
+const drugRoutes = require("./routes/drug.routes");
+drugRoutes(app);
+
+// ------ Middleware
+
+app.listen(port, () => {
+    console.log("Server is running...");
+})
